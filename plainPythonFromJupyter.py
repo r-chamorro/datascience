3c6396fde922a3dbcb53573b8451350c9a609ad9
roger.chamorro <roger.a.chamorro@hotmail.com>
2024-09-26 21:04:30 -0600

    [UPDATE] initializing numpy/pandas testing with mamba env

---
+++ plainPythonFromJupyter.py
@@ -0,0 +1,16 @@
+# %%
+import sys
+import os
+import math
+saludo = "Hola muchachos... awwww!"
+
+# %%
+print(saludo)
+
+# %% [markdown]
+# Esto es texto! Ah caray se da formato de MKD
+
+# %%
+
+# %%
+x = 1
